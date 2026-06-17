8301a8657c593191f58ccab1ec5dd2bb4dc2a701
maiha09 <maiha3069@gmail.com>
2026-06-17 22:19:24 +0700

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def get_base64_image(image_path):
 
 img = ""
 if os.path.exists("data/images.jpg"):
-    img = get_base64_image("data/images.jpg")
+    img = get_base64_image("data/Hinh-1.jpg")
 
 # ===== CSS CẤU HÌNH GIAO DIỆN =====
 st.markdown(
